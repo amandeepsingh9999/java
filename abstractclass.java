e5d81bc344a438347b7c2072e8e48c6aedb98877
amandeepsingh9999 <125952177+amandeepsingh9999@users.noreply.github.com>
2023-09-03 03:15:24 +0000

    Update abstractclass.java

--- abstractclass.java
+++ abstractclass.java
@@ -1,14 +1,1 @@
-abstract class language {
 
-  public void display(){
-    System.out.println("This is the example for abstract class !");
-  }
-} 
-
-public class abstractclass extends language {
-  public static void main(String[] args) {
-    abstractclass obj = new abstractclass();
-    obj.display();
-  }
-  
-}
